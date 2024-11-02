b13e4f49598f16592be6310849ac22f2c9ac3e5e
maxxam123 <hennighausenmax@gmail.com>
2024-11-02 10:59:12 +0000

    Update from https://github.com/maxxam123/platform02/commit/c794a2ca760d84b35ed5d4b220a3aefe0bd5eead

---
+++ 01_infra/10_eks/aws/03_aws_eks/13_application.tf
@@ -0,0 +1,77 @@
+resource "kubectl_manifest" "applications_cluster" {
+  yaml_body = <<-EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app-cluster
+  namespace: argocd
+spec:
+  destination:
+    namespace: argocd
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    path: apps
+    repoURL: https://github.com/maxxam123/upstream.git
+    targetRevision: HEAD
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+      - ServerSideApply=true
+EOF
+}
+
+resource "kubectl_manifest" "secrets_cluster" {
+  yaml_body = <<-EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app-secret
+  namespace: argocd
+spec:
+  destination:
+    namespace: argocd
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    path: secrets
+    repoURL: https://github.com/maxxam123/upstream.git
+    targetRevision: HEAD
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+      - ServerSideApply=true
+EOF
+}
+
+resource "kubectl_manifest" "bootstrap_cluster" {
+  yaml_body = <<-EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app-bootstrap
+  namespace: argocd
+spec:
+  destination:
+    namespace: argocd
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    path: 04_git/aws/10_eks/03_aws_eks/01_bootstrap/02_certmanager
+    repoURL: https://github.com/maxxam123/gitops.git
+    targetRevision: HEAD
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+      - ServerSideApply=true
+EOF
+}
