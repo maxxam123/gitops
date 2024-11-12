3bc1eb26d28fc0edbe60adb52e3ad42bcf9d9ae0
max <81824845+maxxam123@users.noreply.github.com>
2024-11-12 10:30:18 -0800

    Update 03_main.tf

--- 02_tmp/10_eks/aws/03_main.tf
+++ 02_tmp/10_eks/aws/03_main.tf
@@ -312,64 +312,64 @@ provider "kubernetes" {
 }
 
 
-# data "aws_iam_policy_document" "aws_lbc" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "aws_lbc" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["pods.eks.amazonaws.com"]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
 
-#     actions = [
-#       "sts:AssumeRole",
-#       "sts:TagSession"
-#     ]
-#   }
-# }
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
 
-# resource "aws_iam_role" "aws_lbc" {
-#   name               = "${aws_eks_cluster.eks.name}-aws-lbc"
-#   assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
-# }
+resource "aws_iam_role" "aws_lbc" {
+  name               = "${aws_eks_cluster.eks.name}-aws-lbc"
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
+}
 
-# resource "aws_iam_policy" "aws_lbc" {
-#   policy = file("./iam/AWSLoadBalancerController.json")
-#   name   = "AWSLoadBalancerController"
-# }
+resource "aws_iam_policy" "aws_lbc" {
+  policy = file("./iam/AWSLoadBalancerController.json")
+  name   = "AWSLoadBalancerController"
+}
 
-# resource "aws_iam_role_policy_attachment" "aws_lbc" {
-#   policy_arn = aws_iam_policy.aws_lbc.arn
-#   role       = aws_iam_role.aws_lbc.name
-# }
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  policy_arn = aws_iam_policy.aws_lbc.arn
+  role       = aws_iam_role.aws_lbc.name
+}
 
-# resource "aws_eks_pod_identity_association" "aws_lbc" {
-#   cluster_name    = aws_eks_cluster.eks.name
-#   namespace       = "kube-system"
-#   service_account = "aws-load-balancer-controller"
-#   role_arn        = aws_iam_role.aws_lbc.arn
-# }
+resource "aws_eks_pod_identity_association" "aws_lbc" {
+  cluster_name    = aws_eks_cluster.eks.name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.aws_lbc.arn
+}
 
-# resource "helm_release" "aws_lbc" {
-#   name = "aws-load-balancer-controller"
+resource "helm_release" "aws_lbc" {
+  name = "aws-load-balancer-controller"
 
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "kube-system"
-#   version    = "1.7.2"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.2"
 
-#   set {
-#     name  = "clusterName"
-#     value = aws_eks_cluster.eks.name
-#   }
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-# depends_on = [aws_eks_addon.pod_identity]
-# }
+depends_on = [aws_eks_addon.pod_identity]
+}
 
 # data "aws_iam_policy_document" "ebs_csi_driver" {
 #   statement {
