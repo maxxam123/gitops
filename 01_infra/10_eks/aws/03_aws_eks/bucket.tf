3312e654b16c90e8bd0d8f3bf69f4f736cff36c6
maxxam123 <hennighausenmax@gmail.com>
2024-11-14 08:09:51 +0000

    Update from https://github.com/maxxam123/platform02/commit/5996dc45301cbcb86e2cc5bac0c95fabc9c5a5e1

--- 01_infra/10_eks/aws/03_aws_eks/bucket.tf
+++ 01_infra/10_eks/aws/03_aws_eks/bucket.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "velero-bucket-03-aws-eks" {
   bucket = "03-aws-eks-velero-bucket"
 
   tags = {
-    Name        = "03-aws-eks-velero-bucket"
+    Name = "03-aws-eks-velero-bucket"
   }
 }
