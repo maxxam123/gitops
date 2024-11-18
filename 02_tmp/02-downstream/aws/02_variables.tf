59b563b51c2251acaef442b8ff155a1d2f138317
max <81824845+maxxam123@users.noreply.github.com>
2024-11-18 05:48:02 -0800

    Create 02_variables.tf

---
+++ 02_tmp/02-downstream/aws/02_variables.tf
@@ -0,0 +1,17 @@
+variable "cluster_name" {
+    description = "cluster-name"
+    type = string
+    default = "demo"
+}
+
+variable "vpc_name" {
+    description = "vpc-name"
+    type = string
+    default = "dev"
+}
+
+variable "env_name" {
+    description = "env-name"
+    type = string
+    default = "dev"
+}
