2b29eca294dea67614b4d984dd743c30bac96a2f
max <81824845+maxxam123@users.noreply.github.com>
2024-11-19 10:57:21 -0800

    Update 03_main.tf

--- 02_tmp/02-downstream/aws/03_main.tf
+++ 02_tmp/02-downstream/aws/03_main.tf
@@ -335,6 +335,7 @@ resource "aws_iam_role" "aws_lbc" {
 
 resource "aws_iam_policy" "aws_lbc" {
   policy = file("./iam/AWSLoadBalancerController.json")
+  # policy = file("./AWSLoadBalancerController.json")
   name   = "AWSLoadBalancerController"
 }
 
