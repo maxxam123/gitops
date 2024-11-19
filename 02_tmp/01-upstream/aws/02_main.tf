e07eddb7ab637a89818c4a3d82cd05979a725855
max <81824845+maxxam123@users.noreply.github.com>
2024-11-19 10:08:49 -0800

    Create 02_main.tf

---
+++ 02_tmp/01-upstream/aws/02_main.tf
@@ -0,0 +1,265 @@
+locals {
+  eks_version = "1.29"
+  region      = "eu-central-1"
+  zone1       = "eu-central-1a"
+  zone2       = "eu-central-1b"
+  # eks_name    = "demo"
+  # env         = "staging"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.vpc_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
+}
+
+resource "aws_subnet" "private_zone1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = local.zone1
+
+  tags = {
+    "Name"                                                 = "${var.cluster_name}-private-${local.zone1}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${var.cluster_name}-${var.env_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_zone2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = local.zone2
+
+  tags = {
+    "Name"                                                 = "${var.cluster_name}-private-${local.zone2}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${var.cluster_name}-${var.env_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = local.zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${var.cluster_name}-public-${local.zone1}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${var.cluster_name}-${var.env_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_zone2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = local.zone2
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${var.cluster_name}-public-${local.zone2}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${var.cluster_name}-${var.env_name}" = "owned"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.cluster_name}-nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_zone1.id
+
+  tags = {
+    Name = "${var.cluster_name}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public"
+  }
+}
+
+resource "aws_route_table_association" "private_zone1" {
+  subnet_id      = aws_subnet.private_zone1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_zone2" {
+  subnet_id      = aws_subnet.private_zone2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_zone1" {
+  subnet_id      = aws_subnet.public_zone1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_zone2" {
+  subnet_id      = aws_subnet.public_zone2.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_iam_role" "eks" {
+  name = "${var.cluster_name}-${var.env_name}-eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "${var.cluster_name}-${var.env_name}"
+  version  = local.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.private_zone1.id,
+      aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks]
+}
+
+resource "aws_iam_role" "nodes" {
+  name = "${var.cluster_name}-${var.env_name}-eks-nodes"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+# This policy now includes AssumeRoleForPodIdentity for the Pod Identity Agent
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_eks_node_group" "general" {
+  cluster_name    = aws_eks_cluster.eks.name
+  version         = local.eks_version
+  node_group_name = "general"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    aws_subnet.private_zone1.id,
+    aws_subnet.private_zone2.id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.large"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+
+  # Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+}
