2b84d417bc0038ce55667c5b549ea763da1c172b
max <81824845+maxxam123@users.noreply.github.com>
2024-11-21 06:21:11 -0800

    Update 02_variables.tf

--- 02_tmp/01-upstream/aws/02_variables.tf
+++ 02_tmp/01-upstream/aws/02_variables.tf
@@ -1,1 +1,18 @@
 
+variable "cluster-name" {
+    description = "cluster-name"
+    type = string
+    default = "03-aws-upstream"
+}
+
+variable "vpc-name" {
+    description = "vpc-name"
+    type = string
+    default = "03-aws-upstream"
+}
+
+variable "env-name" {
+    description = "env-name"
+    type = string
+    default = "staging"
+}
