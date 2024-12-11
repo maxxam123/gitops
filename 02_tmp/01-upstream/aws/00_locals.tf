1dc15c8e6e23ac2ad36284038cb8f4e504e440c8
max <81824845+maxxam123@users.noreply.github.com>
2024-12-11 07:32:53 -0800

    Create 00_locals.tf

---
+++ 02_tmp/01-upstream/aws/00_locals.tf
@@ -0,0 +1,8 @@
+locals {
+  eks_version = "1.29"
+  region      = "eu-central-1"
+  zone1       = "eu-central-1a"
+  zone2       = "eu-central-1b"
+  # eks_name    = "demo"
+  # env         = "staging"
+}
